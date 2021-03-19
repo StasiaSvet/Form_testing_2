9a52f2775cfb1ef6dfcdb5818d663f7127e7e6be
Stessy <wildorhid@gmail.com>
2021-03-19 23:04:28 +0200

    test: added comments

--- src/test/java/FormDemoqa.java
+++ src/test/java/FormDemoqa.java
@@ -29,9 +29,10 @@ public class FormDemoqa {
         String city = "Jaiselmer";
 
 
-
+        //---Open the page
         open("https://demoqa.com/automation-practice-form");
 
+        //---Data input
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
@@ -39,15 +40,24 @@ public class FormDemoqa {
         $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
 
+
+        //---Calendar date picker
         $(".react-datepicker__month-select").selectOptionByValue("3");
         $(".react-datepicker__year-select").selectOptionByValue("1986");
         $(".react-datepicker__day.react-datepicker__day--002").click();
 
+        //---Subjects
         $("#subjectsInput").setValue(subjectInput1).pressEnter();
         $("#subjectsInput").setValue(subjectInput2).pressEnter();
+
+        //---Hobby
         $(byText("Sports")).click();
         $(byText("Reading")).click();
+
+        //---Uploading picture
         $("#uploadPicture").uploadFromClasspath("111.jpg");
+
+        //---Address
         $("#currentAddress").setValue(cAddress);
         $("#state").click();
         $(byText(state)).click();
@@ -60,6 +70,7 @@ public class FormDemoqa {
 
         $("#submit").click();
 
+        //---Checking the data in the modal form
         $(".modal-content").shouldHave(
 
                 text(firstName + lastName),
